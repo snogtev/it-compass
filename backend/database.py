1670b80cfee818eb774f271575d56a6f43351841
snogtev <stepannogtev2023@gmail.com>
2026-09-17 13:55:15 +0700

    feat: add district and salary fields in table

--- backend/database.py
+++ backend/database.py
@@ -5,6 +5,11 @@ from sqlmodel import Field, SQLModel, create_engine
 class Subject(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str = Field(index=True)
+    district: str = Field(index=True)
+    salary_intern: int
+    salary_junior: int
+    salary_middle: int
+    salary_senior: int
 
 sqlite_file_name = 'subjects.db'
 sqlite_url = f'sqlite:///{sqlite_file_name}'
