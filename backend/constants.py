5f4c27cc3fe23165d289d4ccaf2a52ab85af8eed
snogtev <stepannogtev2023@gmail.com>
2026-09-22 18:36:58 +0700

    feat: add salaries normalization

--- backend/constants.py
+++ backend/constants.py
@@ -1,6 +1,27 @@
-DISTRICTS = ('central', 'northwestern', 'southern', 'north-caucasian', 'volga', 'ural', 'siberian', 'far-eastern')
+DISTRICTS = (
+    'central',
+    'far-eastern',
+    'north-caucasian',
+    'northwestern',
+    'siberian',
+    'southern',
+    'ural',
+    'volga',
+)
 
-GRADES = ('applicant', 'intern', 'junior', 'middle', 'senior')
+GRADES = (
+    'applicant',
+    'intern',
+    'junior',
+    'middle',
+    'senior',
+)
+
+SALARY_FIELDS = {
+    f'salary_{grade}': f'salary_{grade}_score'  
+    for grade in GRADES
+    if grade != 'applicant'
+}
 
 TABLE_URL = 'https://docs.google.com/spreadsheets/d/1-E4EgjpA8y6P0-_NZw2QQFV8Kicgf_G11pMzYV15coo/export?gid=0&format=csv'
 SQLITE_URL = 'sqlite:///subjects.db'
--- backend/data_normalization.py
+++ backend/data_normalization.py
@@ -1,19 +1,21 @@
 from sklearn.preprocessing import minmax_scale
 from sqlmodel import Session, select
 
+from backend.constants import SALARY_FIELDS
 from backend.database import Subject, engine
 
 
 def create_salary_scores():
     with Session(engine) as session:
-        statement = select(Subject.salary_junior)
-        results = session.exec(statement)
-        data = results.all()
-        scores = minmax_scale(data, feature_range=(1, 100)).astype(int).tolist()
 
-        for i, score in enumerate(scores, start=1):
-            subject = session.get(Subject, i)
-            subject.salary_junior_score = score
+        for salary_field, score_field in SALARY_FIELDS.items():
+            statement = select(getattr(Subject, salary_field))
+            results = session.exec(statement)
+            data = results.all()
+            scores = minmax_scale(data, feature_range=(1, 100)).astype(int).tolist()
+            for i, score in enumerate(scores, start=1):
+                subject = session.get(Subject, i)
+                setattr(subject, score_field, score)
 
         session.commit()
         session.refresh(subject)
